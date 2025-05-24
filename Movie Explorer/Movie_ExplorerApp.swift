c3514099ed79b955faf95fd643f91e622d89cdb0
IslomiddinAgzamov <96278128+IslomiddinAgzamov@users.noreply.github.com>
2025-05-24 14:50:28 +0500

    Initial Commit

---
+++ Movie Explorer/Movie_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ExplorerApp.swift
+//  Movie Explorer
+//
+//  Created by Islomiddin on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Movie_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
